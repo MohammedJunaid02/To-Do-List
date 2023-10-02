d4df0e96163ba167c7b4d934d30e28e95596facf
Mohammed Junaid <mohammedjunaidj08@gmail.com>
2023-10-02 22:56:29 +0530

    Added Case Sensitivity and Prevention of Duplicate Tasks to the To-Do List

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+//set was used here to prevent duplicate tasks from getting added
+const taskSet=new Set();
 
 function addTask(){
-    if(inputBox.value === ''){
+    taskName=inputBox.value.trim();
+    if(taskName=== ""){
         alert("You must add some tasks..!!");
+        return;
     }
-    else{
-        let li=document.createElement("li");
-        li.innerHTML=inputBox.value;
-        listContainer.appendChild(li);
-        let span=document.createElement("span");
-        span.innerHTML="\u00d7";
-        li.appendChild(span);
-        saveData();
+
+    if(isTaskDuplicate(taskName)){
+        alert("A task with the same name already exists");
+        return;
     }
+
+    let li=document.createElement("li");
+    li.innerHTML=taskName;
+    li.textContent=taskName;
+    listContainer.appendChild(li);
+
+    let span=document.createElement("span");
+    span.innerHTML="\u00d7";
+    li.appendChild(span);
+
+    taskSet.add(taskName.toLowerCase());
+
+    saveData();
+    
     inputBox.value='';
 }
 
@@ -23,10 +37,24 @@ listContainer.addEventListener("click",function(e){
         saveData();
     }
     else if(e.target.tagName==="SPAN"){
+        const taskName=e.target.parentElement.textContent.trim();
+        taskSet.delete(taskName.toLowerCase());
+        //taskSet.delete(taskName.toUpperCase());
         e.target.parentElement.remove();
         saveData();
     }
 },false);
+
+
+function isTaskDuplicate(taskName){
+    return Array.from(taskSet).some(existingTask=>existingTask.toLowerCase() === taskName.toLowerCase());
+}
+
+function clearTasks(){
+    listContainer.innerHTML="";
+    taskSet.clear();
+    saveData();
+}
 
 function saveData() {
     localStorage.setItem("data",listContainer.innerHTML);
@@ -37,3 +65,4 @@ function showData(){
 }
 
 showData();
+
